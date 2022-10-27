01f4bb5d31cb16fadff31831f73e3c9653a8e3e9
Yue Chang <changy0991@gmail.com>
2022-10-27 11:56:00 -0600

    Added Navigation

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,26 +1,29 @@
 import React from "react";
 
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 import "./UsersList.css";
 
 const UserList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
     <ul className="users-list">
-      {props.items.map(user => (
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.place}
+          placeCount={user.places}
         />
       ))}
     </ul>
